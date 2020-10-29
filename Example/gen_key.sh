72762acebf280bb314a4b36160f2f0abbd4e3573
Moroam <42112985+Moroam@users.noreply.github.com>
2020-10-29 15:46:53 +0300

    Update gen_key.sh

--- Example/gen_key.sh
+++ Example/gen_key.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-openssl req -newkey rsa:2048 -nodes -keyout your_key.key -out your_key.csr  -days 365
+# centos 7
+#openssl req -newkey rsa:2048 -nodes -keyout your_key.key -out your_key.csr  -days 365
+# centos 8
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout your_key.key -out your_key.csr
